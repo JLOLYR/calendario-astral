ece2795dccad0515b4702d7b06470862e265d4fc
julio <j.leyrer01@gmail.com>
2025-07-28 13:02:20 -0400

    Add Firebase messaging service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,13 +5,14 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 
 // Vuelve a pegar tu configuración de Firebase aquí
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
-};
+    apiKey: "AIzaSyAEp97rBImklvm2M0e4yQ0XVO0OQ5LbENg",
+    authDomain: "calendario-astral-popup.firebaseapp.com",
+    projectId: "calendario-astral-popup",
+    storageBucket: "calendario-astral-popup.firebasestorage.app",
+    messagingSenderId: "1010299837934",
+    appId: "1:1010299837934:web:8ace892d1168d47964c2ff",
+    measurementId: "G-ZGW3BLK1W2"
+    };
 
 // Inicializa Firebase
 firebase.initializeApp(firebaseConfig);
